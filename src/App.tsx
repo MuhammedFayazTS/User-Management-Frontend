665171696da265f4fe4aa3e4b9900aa1c99a84aa
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-12-07 10:11:48 +0530

    feat: inital setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Button>Click me</Button>
+    </BrowserRouter>
+  )
+}
+
+export default App
