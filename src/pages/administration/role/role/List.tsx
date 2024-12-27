ab5059ca5aa3a8b1bf8a659b9b311f0021322601
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-12-27 12:30:43 +0530

    feat: create a basic default data table

--- src/pages/administration/role/role/List.tsx
+++ src/pages/administration/role/role/List.tsx
@@ -1,8 +1,44 @@
+import { DefaultTable } from "@/components/core/DefaultTable"
+import { ColumnDef } from "@tanstack/react-table"
 
-const List = () => {
+export type Payment = {
+    id: string
+    amount: number
+    status: "pending" | "processing" | "success" | "failed"
+    email: string
+}
+
+const dummyData = [
+    {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+    },
+]
+
+const columns: ColumnDef<Payment>[] = [
+    {
+        accessorKey: "status",
+        header: "Status",
+    },
+    {
+        accessorKey: "email",
+        header: "Email",
+    },
+    {
+        accessorKey: "amount",
+        header: "Amount",
+    },
+]
+
+const RoleList = () => {
+
     return (
-        <div>List</div>
+        <div className="container mx-auto py-10">
+            <DefaultTable columns={columns} data={dummyData} />
+        </div>
     )
 }
 
-export default List
+export default RoleList
