522f63e52041be1f16994447614d11006fc7d122
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2025-01-17 15:35:23 +0530

    refactor: small updation in select component

--- src/components/core/DefaultSelect.tsx
+++ src/components/core/DefaultSelect.tsx
@@ -1,117 +1,169 @@
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select";
-  import {
     FormControl,
     FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
-  } from "../ui/form";
-  import { FieldValues, Path, UseFormReturn } from "react-hook-form";
-  import { Skeleton } from "../ui/skeleton";
-  
-  export type SelectOption = {
+} from "../ui/form";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectSeparator,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+import { Skeleton } from "../ui/skeleton";
+import { useEffect, useRef, useState } from "react";
+import { Input } from "../ui/input";
+
+export type SelectOption = {
     label: string;
     value: number;
-  };
-  
-  interface IDefaultSelectProps<TFormValues extends FieldValues> {
+};
+
+interface IDefaultSelectProps<TFormValues extends FieldValues> {
     name: Path<TFormValues>;
     control: UseFormReturn<TFormValues>["control"];
     label?: string;
     placeholder?: string;
-    width?: number | "full";
-    minWidth?: number | "full";
-    maxWidth?: number | "full";
+    width?: string;
+    minWidth?: string;
+    maxWidth?: string;
     message?: string;
     options?: SelectOption[];
     isLoading?: boolean;
     disabled?: boolean;
     onChangeCallback?: (value: number | null) => void; // Callback for handling value changes
-  }
-  
-  export const DefaultSelect = <TFormValues extends FieldValues>({
+}
+
+export const DefaultSelect = <TFormValues extends FieldValues>({
     name,
     control,
     label,
     placeholder = "Select an option",
-    width,
+    width = "100%",
     minWidth,
     maxWidth,
     message,
-    options,
+    options = [],
     isLoading = false,
     disabled = false,
     onChangeCallback,
-  }: IDefaultSelectProps<TFormValues>) => {
+}: IDefaultSelectProps<TFormValues>) => {
+    const [search, setSearch] = useState("");
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const filteredOptions = search
+        ? options.filter((option) =>
+            option.label.toLowerCase().startsWith(search.toLowerCase())
+        ) || []
+        : options;
+
+    useEffect(() => {
+        // Focus the input field when the dropdown opens
+        const handleFocus = () => {
+            inputRef.current?.focus();
+        };
+
+        document.addEventListener("mousedown", handleFocus);
+
+        return () => {
+            document.removeEventListener("mousedown", handleFocus);
+        };
+    }, []);
+
     return (
-      <FormField
-        control={control}
-        name={name}
-        disabled={disabled}
-        render={({ field }) => {
-          const handleValueChange = (value: string | undefined) => {
-            const numberValue = value ? parseInt(value, 10) : null;
-            field.onChange(numberValue);
-            if (onChangeCallback) {
-              onChangeCallback(numberValue);
-            }
-          };
-  
-          return (
-            <FormItem style={{ width, minWidth, maxWidth }}>
-              {label && <FormLabel>{label}</FormLabel>}
-              {isLoading ? (
-                <Skeleton
-                  className={`h-10 ${width ? `w-${width}` : "w-full"}`}
-                />
-              ) : (
-                <>
-                  <Select
-                    onValueChange={handleValueChange}
-                    defaultValue={field.value?.toString()}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder={placeholder} />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent side="bottom">
-                      {options && options?.length > 0 ? (
-                        <SelectGroup>
-                          {options.map((option) => (
-                            <SelectItem
-                              key={option.value}
-                              value={option.value.toString()}
-                            >
-                              {option.label}
-                            </SelectItem>
-                          ))}
-                        </SelectGroup>
-                      ) : (
-                        <SelectGroup>
-                          <SelectItem value="" disabled>
-                            No options available
-                          </SelectItem>
-                        </SelectGroup>
-                      )}
-                    </SelectContent>
-                  </Select>
-                  {message && <FormDescription>{message}</FormDescription>}
-                  <FormMessage />
-                </>
-              )}
-            </FormItem>
-          );
-        }}
-      />
+        <FormField
+            control={control}
+            name={name}
+            disabled={disabled}
+            render={({ field }) => {
+                const handleValueChange = (value: string | undefined) => {
+                    const numberValue = value ? parseInt(value, 10) : null;
+                    field.onChange(numberValue);
+                    if (onChangeCallback) {
+                        onChangeCallback(numberValue);
+                    }
+                };
+
+                const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+                    const value = e.target?.value || "";
+                    setSearch(value);
+                };
+
+                return (
+                    <FormItem
+                        className="flex flex-col"
+                        style={{ width, minWidth, maxWidth }}
+                    >
+                        {label && <FormLabel>{label}</FormLabel>}
+                        {isLoading ? (
+                            <Skeleton
+                                className={`h-10 ${width ? `w-${width}` : "w-full"
+                                    }`}
+                            />
+                        ) : (
+                            <>
+                                <Select
+                                    onValueChange={handleValueChange}
+                                    defaultValue={field.value?.toString()}
+                                    onOpenChange={() => {
+                                        setSearch("");
+                                        inputRef.current?.focus();
+                                    }}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue
+                                                placeholder={placeholder}
+                                            />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent side="bottom">
+                                        <SelectGroup>
+                                            <Input
+                                                ref={inputRef}
+                                                type="text"
+                                                onChange={handleSearch}
+                                                placeholder="Search..."
+                                            />
+                                            <SelectSeparator />
+                                        </SelectGroup>
+                                        {filteredOptions &&
+                                            filteredOptions?.length > 0 ? (
+                                            <SelectGroup>
+                                                {filteredOptions.map(
+                                                    (option) => (
+                                                        <SelectItem
+                                                            key={option.value}
+                                                            value={option.value?.toString()}
+                                                            className="flex justify-between items-center"
+                                                        >
+                                                            {option.label}
+                                                        </SelectItem>
+                                                    )
+                                                )}
+                                            </SelectGroup>
+                                        ) : (
+                                            <div className="px-4 py-2 text-sm text-gray-500">
+                                                No options available
+                                            </div>
+                                        )}
+                                    </SelectContent>
+                                </Select>
+                                {message && (
+                                    <FormDescription>{message}</FormDescription>
+                                )}
+                                <FormMessage />
+                            </>
+                        )}
+                    </FormItem>
+                );
+            }}
+        />
     );
-  };
-  
+};
